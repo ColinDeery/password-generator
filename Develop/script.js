03b0cd6733c67211319ded850f0f7fb8a9b0ea44
ColinDeery <colinbdeery@gmail.com>
2022-11-10 20:46:31 -0500

    Added js for character length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var charLength = window.prompt("How many characters would you like your password to be? (Min 8 Max 128)");
+
+  if (charLength < 8 || charLength > 128) {
+    window.alert("Password length must be at least 8 characters and no more than 128 characters."); return;
+  }
+
+  if (isNaN(charLength)) {
+    window.alert("Select a password length between 8 and 128 characters.");
+    return;
+  }
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  // var charLength = window.prompt("How many characters would you like your password to be? (Min 8 Max 128)");
+
+  // if (charLength < 8 || charLength > 128) {
+  //   window.alert("Password length must be at least 8 characters and no more than 128 characters."); return;
+  // }
+
+  // if (isNaN(charLength)) {
+  //   window.alert("Select a password length between 8 and 128 characters.");
+  //   return;
+  // }
+  
   passwordText.value = password;
 
 }
