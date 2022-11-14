b4e3c37490e4944b0f2c21899027dfacffe30216
ColinDeery <colinbdeery@gmail.com>
2022-11-14 09:27:58 -0500

    fixed js file to include spread operator

--- script.js
+++ script.js
@@ -26,54 +26,59 @@ function generatePassword() {
 
   // WHEN asked for character types to include in the password
   // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-  
+
   var includeLower = window.confirm("Would you like your password to include lowercase characters?")
   var includeUpper = window.confirm("Would you like your password to include uppercase characters?")
   var includeNumeric = window.confirm("Would you like your password to include numbers?")
   var includeSpecial = window.confirm("Would you like your password to include special characters?")
-  
+
   var charSetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
   var charSetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
   var charSetNumeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-  var charSetSpecial = [" ", "!", "''", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~" ]
+  var charSetSpecial = [" ", "!", "''", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
 
   var charSet = []
 
   if (includeLower === true) {
-    charSet.push(charSetLower)
+    charSet.push(...charSetLower)
   }
 
   if (includeUpper === true) {
-    charSet.push(charSetUpper)
+    charSet.push(...charSetUpper)
   }
 
   if (includeNumeric === true) {
-    charSet.push(charSetNumeric)
+    charSet.push(...charSetNumeric)
   }
 
   if (includeSpecial === true) {
-    charSet.push(charSetSpecial)
+    charSet.push(...charSetSpecial)
   }
 
+  console.log(charSet)
+
+  //randomize the charSet array
   charSet.sort(() => Math.random() - 0.5);
 
+  console.log(charSet)
+
+  for (var i = 0; i < charLength; i += 3) {
+    console.log(charSet[i])
+
+  }
+  //pick charLength number of charaters from the randomized charSet array
+  //starting from 0, ending with charLength number
   var passwordSet = charSet.slice(0, charLength);
 
+  console.log(passwordSet)
+
+  //combine array items into a string
   var lastPass = (passwordSet.join(''));
 
-  return(lastPass);
-
-  // var generatedPassword = ""
-
-  // for (var i = 0; i < charLength; i++) {
-  //   Math.random()
-
-  // }
-    
-  
+  return (lastPass);
 
 }
 
